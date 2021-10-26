6a6a92afed76d460f7922cc02734c9f65e4dfe94
loveyousomuch <loveyousomuch554@gmail.com>
2021-10-26 22:36:57 +0300

    check for config existing

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-# set .env vars
-export $(echo $(cat ./config/.env | sed 's/#.*//g'| xargs) | envsubst)
+FILE=./config/.env
+
+if test -f "$FILE"; then
+  # set .env vars
+  export $(echo $(cat $FILE | sed 's/#.*//g'| xargs) | envsubst)
+else
+  echo "Config file \"$FILE\" not found."
+fi
 
 # run bot
 python3 src/main.py
